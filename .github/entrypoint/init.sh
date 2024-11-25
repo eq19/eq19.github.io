e98888b0bf5c17d0a8ca5c8f1bcfd465073a68f6
eq19 <eq19@users.noreply.github.com>
2024-11-25 13:54:09 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -66,19 +66,23 @@ if [[ "${JOB_ID}" == "3" ]]; then
 
   cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* .
 
-elif [[ "${JOB_ID}" == "4" ]]; then
+elif [[ "${JOB_ID}" == "4" ]] && [[ "${WIKI}" != "${BASE}" ]]; then
 
   echo -e "\n$hr\nWORKSPACE\n$hr"
+  rm -rf ${RUNNER_TEMP//\\//}/wikidir
 
-  if [[ "${WIKI}" != "${BASE}" ]]; then
-    git clone $WIKI ${RUNNER_TEMP}/wikidir
-    mv -f ${RUNNER_TEMP}/wikidir/Home.md ${RUNNER_TEMP}/wikidir/README.md
-    find ${GITHUB_WORKSPACE} -type d -name "${FOLDER}" -prune -exec sh -c 'wiki.sh "$1"' sh {} \;
-  fi
+  git clone $WIKI ${RUNNER_TEMP//\\//}/wikidir
+  cd ${RUNNER_TEMP//\\//}/wikidir && mv -f Home.md README.md
 
-  find ${GITHUB_WORKSPACE} -type d -name "${FOLDER}" -prune -exec sh -c 'cat ${TEMP_FOLDER}/README.md >> $1/README.md' sh {} \;
-  find ${GITHUB_WORKSPACE} -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
-    
+  find ${GITHUB_WORKSPACE//\\//} -type d -name "${FOLDER}" -prune -exec sh -c 'wiki.sh "$1"' sh {} \;
+  find ${GITHUB_WORKSPACE//\\//} -type d -name "${FOLDER}" -prune -exec sh -c 'cat ${RUNNER_TEMP//\\//}/README.md >> $1/README.md' sh {} \;
+  find ${GITHUB_WORKSPACE//\\//} -type d -name "${FOLDER}" -prune -exec sh -c 'ls -alR' sh {} \;
+
+  #echo "action_state=yellow" | Out-File -FilePath $env:GITHUB_ENV -Append # no need for -Encoding utf8
+  find ${GITHUB_WORKSPACE//\\//} -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
+
+  exit 1
+
 fi
 
 if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
